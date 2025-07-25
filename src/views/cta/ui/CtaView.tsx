8b0958c155f02b1d04195ae01c3ab13675cd3a4e
RustamOralkanov <oralkanov1r@icloud.com>
2025-07-25 08:53:46 +0500

    added to git

--- src/views/cta/ui/CtaView.tsx
+++ src/views/cta/ui/CtaView.tsx
@@ -23,14 +23,18 @@ export const CtaView = () => {
             <Row className="max-lg:!flex-col-reverse">
                 <Col xl={10} lg={24} md={24} sm={24} xs={24} className="bg-orange">
                     <div className="flex flex-col h-full gap-40 bg-orange p-60 -mt-45 max-lg:mt-0 max-lg:p-24 max-lg:gap-30">
-                        <h4 className="uppercase text-[40px] -tracking-[0.01em] text-white font-light leading-tight font-display max-lg:text-2xl">{data?.title}</h4>
-                        <p className="text-white uppercase text-[16px] leading-snug max-w-320 font-display max-lg:text-sm">{data?.subtitle}</p>
+                        <h4 className="uppercase text-[40px] -tracking-[0.01em] text-white font-light leading-tight font-display max-lg:text-2xl">
+                            {data?.title}
+                        </h4>
+                        <p className="text-white uppercase text-[16px] leading-snug max-w-320 font-display max-lg:text-sm">
+                            {data?.subtitle}
+                        </p>
                         <CtaForm />
                     </div>
                 </Col>
                 <Col xl={14} lg={24} md={24} sm={24} xs={24}>
                     <div className="w-full h-full max-lg:!-mt-200">
-                        <img src="/day.webp" className="w-full h-full max-lg:max-h-200 object-cover" />
+                        <img src="/night.webp" className="w-full h-full max-lg:max-h-200 object-cover" />
                     </div>
                 </Col>
             </Row>
