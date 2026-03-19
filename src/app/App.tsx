99907eaf495e7aedd75eecc62802fb5bf289f74f
rustam <oralkanov1r@icloud.com>
2026-03-19 15:03:32 +0500

    remove redirect

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,59 +1,53 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { OrientData } from "@/shared/model";
-import { AboutView } from "@/views/about";
-import { ArchitectureView } from "@/views/architecture";
-import { CompanyView } from "@/views/company";
+import { OrientData } from "@/shared/model"
+import { AboutView } from "@/views/about"
+import { ArchitectureView } from "@/views/architecture"
+import { CompanyView } from "@/views/company"
+import axios from "axios"
+import { useEffect, useState } from "react"
 // import { CtaView } from "@/views/cta";
-import { HallView } from "@/views/hall";
-import { ImprovementView } from "@/views/improvement";
+import { HallView } from "@/views/hall"
+import { ImprovementView } from "@/views/improvement"
 // import { LayoutsView } from "@/views/layouts";
-import { LocationView } from "@/views/location";
-import { MainView } from "@/views/main";
-import { ParkingView } from "@/views/parking";
-import { ServiceView } from "@/views/service";
-import { Footer } from "@/widgets/footer";
-import { OrientProvider } from "@/shared/provider";
-import { ParallaxProvider } from "react-scroll-parallax";
-import { Booking } from "@/shared/ui/booking";
+import { OrientProvider } from "@/shared/provider"
+import { Booking } from "@/shared/ui/booking"
+import { LocationView } from "@/views/location"
+import { MainView } from "@/views/main"
+import { ParkingView } from "@/views/parking"
+import { ServiceView } from "@/views/service"
+import { Footer } from "@/widgets/footer"
+import { ParallaxProvider } from "react-scroll-parallax"
 
 export const App = () => {
-    const [data, setData] = useState<OrientData["ru"] | null>(null);
+	const [data, setData] = useState<OrientData["ru"] | null>(null)
 
-    useEffect(() => {
-        axios
-            .get("/orient.json")
-            .then((response) => {
-                setData(response.data.ru);
-            })
-            .catch((error) => {
-                console.error("Ошибка при загрузке JSON:", error);
-            });
-    }, []);
+	useEffect(() => {
+		axios
+			.get("/orient.json")
+			.then(response => {
+				setData(response.data.ru)
+			})
+			.catch(error => {
+				console.error("Ошибка при загрузке JSON:", error)
+			})
+	}, [])
 
-    useEffect(() => {
-        window.location.href = "https://ab-capital.kz";
-    }, []);
-
-    return <div>Redirecting...</div>;
-
-    return (
-        <ParallaxProvider>
-            <OrientProvider>
-                <MainView {...data} />
-                <AboutView {...data?.about} />
-                <LocationView {...data?.location} />
-                <ImprovementView {...data?.improvement} />
-                <ArchitectureView {...data?.architecture} />
-                <HallView {...data?.hall} />
-                <ParkingView {...data?.parking} />
-                {/* <LayoutsView {...data?.layouts} /> */}
-                {/* <CtaView /> */}
-                <CompanyView {...data?.company} />
-                <ServiceView {...data?.service} />
-                <Footer {...data?.footer} />
-                <Booking />
-            </OrientProvider>
-        </ParallaxProvider>
-    );
-};
+	return (
+		<ParallaxProvider>
+			<OrientProvider>
+				<MainView {...data} />
+				<AboutView {...data?.about} />
+				<LocationView {...data?.location} />
+				<ImprovementView {...data?.improvement} />
+				<ArchitectureView {...data?.architecture} />
+				<HallView {...data?.hall} />
+				<ParkingView {...data?.parking} />
+				{/* <LayoutsView {...data?.layouts} /> */}
+				{/* <CtaView /> */}
+				<CompanyView {...data?.company} />
+				<ServiceView {...data?.service} />
+				<Footer {...data?.footer} />
+				<Booking />
+			</OrientProvider>
+		</ParallaxProvider>
+	)
+}
